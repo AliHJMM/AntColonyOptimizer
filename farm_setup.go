f751a198c42bf28da018ad653fbc6474c030166d
Ali <AliHasanJMM@gmail.com>
2024-07-28 17:06:47 +0300

    ColonyWorker Struct

--- farm_setup.go
+++ farm_setup.go
@@ -1,3 +1,12 @@
 package functions
 
 import "math"
+
+// ColonyWorker represents an individual ant in the colony
+type ColonyWorker struct {
+	antNumber        int            // Unique identifier for the ant
+	currentRoom      *AntRoom       // The room where the ant is currently located
+	visitedRoom      map[*AntRoom]bool // Keeps track of rooms visited by the ant
+	inMotion         bool           // Indicates if the ant is currently moving
+	hasCompletedMove bool           // Indicates if the ant has finished its move
+}

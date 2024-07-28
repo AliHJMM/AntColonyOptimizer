1a4007fb9f1294a4a599f913ee978657237bd4be
Ali <AliHasanJMM@gmail.com>
2024-07-28 17:04:06 +0300

    RepositionAnts Function

--- functions/ant_management.go
+++ functions/ant_management.go
@@ -1,2 +1,21 @@
 package functions
 
+func (colony *Farm) RepositionAnts() {
+	// Reset all ants to their initial position
+	for i := 0; i < colony.antCount; i++ {
+			// Set current room to initial room for each ant
+			colony.workers[i].currentRoom = colony.initialRoom
+
+			// Clear visited room history for each ant
+			for roomKey := range colony.workers[i].visitedRoom {
+					colony.workers[i].visitedRoom[roomKey] = false
+			}
+			// Mark initial room as not visited
+			colony.workers[i].visitedRoom[colony.initialRoom] = false
+			// Set ant status to "in motion"
+			colony.workers[i].inMotion = true
+	}
+	// Reset all tunnel access after repositioning ants
+	colony.ResetTunnelAccess()
+}
+

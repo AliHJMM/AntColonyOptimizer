a88ba889eab2479a4b977bc7037307fa0a2487e3
Ali <AliHasanJMM@gmail.com>
2024-07-28 17:15:55 +0300

    pathFinding file

--- functions/pathFinding.go
+++ functions/pathFinding.go
@@ -6,3 +6,11 @@ import (
 	"os"
 	"sort"
 )
+
+var movementOccurred = false // Global flag to track if any ant movement occurred
+
+// printAndExit prints a message and exits the program with the given exit code
+func printAndExit(message string, exitCode int) {
+    fmt.Println(message)
+    os.Exit(exitCode)
+}

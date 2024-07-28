17356ffd1f5a2c6e272c500769ea45e305c1a10f
Ali <AliHasanJMM@gmail.com>
2024-07-28 17:14:46 +0300

    Formatting file

--- functions/formatting.go
+++ functions/formatting.go
@@ -6,5 +6,17 @@ import (
 )
 
 func (colony *Farm) FormatAntLocations() string {
+// Initialize an empty string to store formatted locations
+formattedLocations := ""
 
+// Create a new slice to hold ordered ants
+orderedAnts := make([]*ColonyWorker, colony.antCount)
+
+// Copy the workers to the new slice
+copy(orderedAnts, colony.workers)
+
+// Sort the ants by their number
+sort.SliceStable(orderedAnts, func(first, second int) bool {
+		return orderedAnts[first].antNumber < orderedAnts[second].antNumber
+})
 }

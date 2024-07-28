6673b1a416a03808fc718218c80674ecc189b482
Ali <AliHasanJMM@gmail.com>
2024-07-28 17:20:52 +0300

    room_management file

--- functions/room_management.go
+++ functions/room_management.go
@@ -1,1 +1,9 @@
 package functions
+
+// CountTunnels counts the number of tunnels leading to rooms closer to the destination
+func (farm *Farm) CountTunnels(chamber *AntRoom) int {
+	// Initialize the tunnel count
+	tunnelCount := 0
+
+	// Start with the first connected room
+	connection := chamber.connectedRooms.firstNode

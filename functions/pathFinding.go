75c9f9d66a20bdac973657e755d0a7758c816397
Ali <AliHasanJMM@gmail.com>
2024-07-28 17:16:06 +0300

    pathFinding file

--- functions/pathFinding.go
+++ functions/pathFinding.go
@@ -14,3 +14,15 @@ func printAndExit(message string, exitCode int) {
     fmt.Println(message)
     os.Exit(exitCode)
 }
+
+// FindShortestPath calculates the shortest path from the final room to all other rooms
+func (farm *Farm) FindShortestPath() {
+    stack := &RoomStack{}
+    exploredRooms := make(map[*AntRoom]bool)
+    seenRooms := make(map[*AntRoom]bool)
+
+    // Start from the final room
+    stack.AddRoomToStack(farm.finalRoom)
+    current := stack.RemoveRoom()
+    farm.roomPaths[current] = 1
+    seenRooms[current] = true

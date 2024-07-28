d3d19201e1370beb08a73aed047d2921b3701bcc
Ali <AliHasanJMM@gmail.com>
2024-07-28 17:15:32 +0300

    pathFinding file

--- functions/formatting.go
+++ functions/formatting.go
@@ -17,22 +17,21 @@ func (colony *Farm) FormatAntLocations() string {
 
 	// Sort the ants by their number
 	sort.SliceStable(orderedAnts, func(first, second int) bool {
-			return orderedAnts[first].antNumber < orderedAnts[second].antNumber
+		return orderedAnts[first].antNumber < orderedAnts[second].antNumber
 	})
 
 	// Iterate through the sorted ants
 	for _, worker := range orderedAnts {
-			// Check if the ant has completed a move
-			if worker.hasCompletedMove {
-					// Format the ant's location and add it to the string
-					formattedLocations += fmt.Sprintf("L%d-%s ", worker.antNumber, worker.currentRoom.roomName)
+		// Check if the ant has completed a move
+		if worker.hasCompletedMove {
+			// Format the ant's location and add it to the string
+			formattedLocations += fmt.Sprintf("L%d-%s ", worker.antNumber, worker.currentRoom.roomName)
 
-					// Reset the move completion flag
-					worker.hasCompletedMove = false
-			}
+			// Reset the move completion flag
+			worker.hasCompletedMove = false
+		}
 	}
 
 	// Return the formatted locations with a newline
 	return formattedLocations + "\n"
 }
-
